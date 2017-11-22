a0dc2c7e81d37ec755764a79bf96b95c27cc0324
star-citizen <Christoph07@gmail.com>
2017-11-22 14:55:16 -0500

    Initial commit

---
+++ src/playground/es6letconst.js
@@ -0,0 +1,8 @@
+var nameVar = 'Andrew';
+console.log('nameVar', nameVar);
+
+let nameLet = 'Jen';
+console.log('nameLet', nameLet);
+
+const nameConst = 'Frank';
+console.log('nameConst', nameConst);
