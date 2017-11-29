180c07cfe7e14039415fc22293820000de3cc282
star-citizen <Christoph07@gmail.com>
2017-11-29 17:52:39 -0500

    webpack/updates

---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import Header from './Header';
+import Action from './Action';
+import AddOption from './AddOption';
+import Options from './Options';
+import OptionModal from './OptionModal';
+
+class IndecisionApp extends React.Component {
+    state = {
+      options: [],
+      selectedOption: undefined
+    };
+    handleCloseModal = () => {
+      this.setState(() => ({
+        selectedOption: undefined
+      }));
+    }
+    handleDeleteOptions = () => {
+      this.setState(() =>  ({
+        options: []
+      }));
+    };
+    handleDeleteOption = (optionToRemove) => {
+      this.setState((prevState) => ({
+        options: prevState.options.filter((option) => {
+          return optionToRemove !== option;
+        })
+      }));
+    };
+    handlePick = () => {
+      const randomNum = Math.floor(Math.random() * this.state.options.length);
+      const option = this.state.options[randomNum];
+      this.setState(() => ({
+        selectedOption: option
+      }));
+    };
+    handleAddOption = (option) => {
+      if (!option) {
+        return 'Invalid value';
+      } else if (this.state.options.indexOf(option) > -1) {
+        return 'Duplicate entry';
+      }
+
+      this.setState((prevState) => ({
+        options: prevState.options.concat([option])
+      }));
+    };
+
+    componentDidMount() {
+      try {
+        const json = localStorage.getItem('options');
+        const options = JSON.parse(json);
+
+        if (options) {
+          this.setState(() => ({ options }));
+        }
+      } catch (e) {
+        //Do nothing
+      }
+    }
+    componentDidUpdate(prevProps, prevState) {
+      if (prevState.options.length !== this.state.options.length) {
+        const json = JSON.stringify(this.state.options);
+        localStorage.setItem('options', json);
+      }
+    }
+    componentWillUnmount() {
+      console.log('unmount');
+    }
+    render() {
+      const subtitle = 'Put your life in the hands of a computer';
+
+      return (
+        <div>
+          <Header subtitle={subtitle} />
+          <Action
+            handlePick={this.handlePick}
+            hasOptions={this.state.options.length > 0}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+          <OptionModal
+            selectedOption={this.state.selectedOption}
+            handleCloseModal={this.handleCloseModal}
+          />
+        </div>
+      );
+    }
+}
+
+export default IndecisionApp;
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,8 +5,23 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: props.count
+      count: 0
     };
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
+    }
+  }
+  componentDidMount() {
+    const stringCount = localStorage.getItem('count');
+    const count = parseInt(stringCount, 10);
+
+    if (!isNaN(count)) {
+      this.setState(() => ({
+        count
+      }));
+    }
   }
   handleAddOne() {
     this.setState((prevState) => {
@@ -40,10 +55,6 @@ class Counter extends React.Component {
     );
   }
 }
-
-Counter.defaultProps = {
-  count: 0
-};
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
 
