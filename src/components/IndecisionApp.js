803566390108f3af34fc007b49bb93744d60edcd
star-citizen <Christoph07@gmail.com>
2017-11-30 16:37:53 -0500

    final

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -73,18 +73,22 @@ class IndecisionApp extends React.Component {
       return (
         <div>
           <Header subtitle={subtitle} />
+          <div className="container">
           <Action
             handlePick={this.handlePick}
             hasOptions={this.state.options.length > 0}
           />
-          <Options
-            options={this.state.options}
-            handleDeleteOptions={this.handleDeleteOptions}
-            handleDeleteOption={this.handleDeleteOption}
-          />
-          <AddOption
-            handleAddOption={this.handleAddOption}
-          />
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
+          </div>
           <OptionModal
             selectedOption={this.state.selectedOption}
             handleCloseModal={this.handleCloseModal}
